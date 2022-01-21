a7c6e4070d4e949b4e532776997c8ffda3dc7952
Sasmita Sahu <sasmitasahu2394@gmail.com>
2022-01-21 18:18:33 +0530

    NetRefresher Assignmant

---
+++ LMS/NetRefresherAssignment-NetBackendModuleAssignment/ConsoleApp13/ConsoleApp13/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    public class Department
+    {
+        public string GetDepartmentName()
+        {
+            return "IT Department";
+        }
+        public int GetNumberOfDepartments()
+        {
+            return 50 ;
+        }
+        public  virtual string GetDepartmentDetails()
+        {
+            return "Software";
+        }
+    }
+}
---
+++ LMS/NetRefresherAssignment-NetBackendModuleAssignment/ConsoleApp13/ConsoleApp13/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+   public class Employee : Department
+    {
+        public string GetEmployeeFirstName()
+        {
+            return "Sasmita";
+        }
+        public string GetEmployeeLastName()
+        {
+            return "Sahu";
+        }
+        public override string GetDepartmentDetails()
+        {
+            return"Software";
+        }
+    };
+}
