4e7c688325d1d093b386fd585f5760d678831fe1
Sasmita Sahu <sasmitasahu2394@gmail.com>
2022-01-21 18:24:17 +0530

    NetBackendModuleAssignment

---
+++ LMS/NetRefresherAssignment-NetBackendModuleAssignment/WebApplication32/WebApplication32/Data/WebApplication32Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication32.Models;
+
+namespace WebApplication32.Data
+{
+    public class WebApplication32Context : DbContext
+    {
+        public WebApplication32Context (DbContextOptions<WebApplication32Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication32.Models.Student> Student { get; set; }
+    }
+}
