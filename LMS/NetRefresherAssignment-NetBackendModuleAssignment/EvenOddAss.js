1a0065771a1cd22eff533b36253e2d41755b03c2
Sasmita Sahu <sasmitasahu2394@gmail.com>
2022-01-24 09:36:02 +0530

    Javascript-JqueryAssignment

---
+++ LMS/NetRefresherAssignment-NetBackendModuleAssignment/EvenOddAss.js
@@ -0,0 +1,18 @@
+var numbers = [];
+
+for (var i=1; i <=50; i++){
+
+   numbers.push(i);
+
+   if (i % 2 === 0){
+   console.log(+i + " is even no");
+
+
+   }
+
+   else {
+   console.log (+i + " is odd no");   
+   }
+
+ }
+
